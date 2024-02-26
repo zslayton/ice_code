ba7efa64365506839e08ac6b4a38417dfe9183c0
Zack Slayton <zslayton@amazon.com>
2024-02-26 17:24:18 -0500

    rustfmt, adds scope to labeled expansion

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,25 @@
 #[macro_export]
 macro_rules! ice {
-  // If the invocation includes a label...
-  ($label:ident => $expr:expr) => {
-      // ...define a named function whose definition will never be inlined...
-    #[cold]
-    #[inline(never)]
-    fn $label<T, F: FnOnce() -> T>(f: F) -> T {
-        f()
-    }
-      // ...and then wrap our `expr` in a closure and pass it to the function.
-    $label(|| $expr)
-  };
+    // If the invocation includes a label...
+    ($label:ident => $expr:expr) => {{
+        // ...define a named function whose definition will never be inlined...
+        #[cold]
+        #[inline(never)]
+        fn $label<T, F: FnOnce() -> T>(f: F) -> T {
+            f()
+        }
+        // ...and then wrap our `expr` in a closure and pass it to the function.
+        $label(|| $expr)
+    }};
     // If no label is specified, just define a closure to do the same thing.
-  ($expr:expr) => {
-    {
+    ($expr:expr) => {{
         // Closures that are passed in argument position are allowed to have annotations for
         // obscure historical reasons.
-      let mut closure = core::convert::identity(#[cold] #[inline(never)] || $expr);
-      closure()
-    }
-  };
+        let mut closure = core::convert::identity(
+            #[cold]
+            #[inline(never)]
+            || $expr,
+        );
+        closure()
+    }};
 }
-
